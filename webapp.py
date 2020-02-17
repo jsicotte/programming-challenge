066db97fadc7e6efa197d2ebe25bd9888c4731a8
Jason Sicotte <sicotte.jason@gmail.com>
2020-02-17 11:17:26 -0600

    adding REST interface

---
+++ webapp.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import request
+from Scores import DistanceScore
+from TextScore import text_score
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def hello():
+    document_a = request.json['document_a']
+    document_b = request.json['document_b']
+    distance_score = text_score(document_a, document_b)
+    return {
+        "distance_score": {
+            "edit_distance": f"{distance_score.edit_distance}",
+            "max_distance": f"{distance_score.max_score}",
+            "score": f"{distance_score.percentage_score()}"
+        }
+    }
+    return DistanceScore(1,2)
+
+if __name__ == '__main__':
+    app.run()
